f8f2c0de493e4e68af807a64d1004a101f988005
jmeister <git@joergmeister.de>
2015-01-05 00:07:07 +0100

    Test - Gui und Model laufen auseinander

---
+++ test/java/org/nocket/test/eager/validation/EagerValidation2Test.java
@@ -0,0 +1,76 @@
+package org.nocket.test.eager.validation;
+
+import gengui.annotations.Eager;
+
+import java.io.Serializable;
+
+import javax.validation.constraints.Size;
+
+public class EagerValidation2Test implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private boolean toogler;
+	private String text;
+	private String textEager;
+
+	private String kontonummer;
+	private String blz;
+
+	public boolean getToogler() {
+		return toogler;
+	}
+
+	@Eager
+	public void setToogler(boolean toogler) {
+		this.toogler = toogler;
+		kontonummer = null;
+		blz = null;
+	}
+
+	public String getKontonummer() {
+		return kontonummer;
+	}
+
+	public void setKontonummer(String kontonummer) {
+		this.kontonummer = kontonummer;
+	}
+
+	public String disableKontonummer() {
+		return toogler ? "" : null;
+	}
+
+	public String getBlz() {
+		return blz;
+	}
+
+	public void setBlz(String blz) {
+		this.blz = blz;
+	}
+
+	public String disableBlz() {
+		return toogler ? "" : null;
+	}
+
+	@Size(max = 5)
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public String getTooglerResult() {
+		return toogler ? "gesetzt" : "nicht gesetzt";
+	}
+
+	public String getTextEager() {
+		return textEager;
+	}
+
+	@Eager
+	public void setTextEager(String textEager) {
+		this.textEager = textEager;
+	}
+}
---
+++ test/java/org/nocket/test/eager/validation/EagerValidation2TestConstants.java
@@ -0,0 +1,39 @@
+package org.nocket.test.eager.validation;
+
+// CHECKSTYLE_OFF
+public final class EagerValidation2TestConstants {
+
+    private EagerValidation2TestConstants() {}
+
+    /** SimplePropertyElement: Blz */
+    public static final String Blz = "Blz";
+    /** SimplePropertyElement: Kontonummer */
+    public static final String Kontonummer = "Kontonummer";
+    /** SimplePropertyElement: Text */
+    public static final String Text = "Text";
+    /** SimplePropertyElement: Text Eager */
+    public static final String TextEager = "TextEager";
+    /** CheckboxPropertyElement: Toogler */
+    public static final String Toogler = "Toogler";
+    /** SimplePropertyElement: Toogler Result */
+    public static final String TooglerResult = "TooglerResult";
+
+    public final class Properties {
+
+        private Properties() {}
+
+        /** Blz */
+        public static final String Blz = "Blz";
+        /** Kontonummer */
+        public static final String Kontonummer = "Kontonummer";
+        /** Text */
+        public static final String Text = "Text";
+        /** Text Eager */
+        public static final String TextEager = "TextEager";
+        /** Toogler */
+        public static final String Toogler = "Toogler";
+        /** Toogler Result */
+        public static final String TooglerResult = "TooglerResult";
+    }
+
+}
